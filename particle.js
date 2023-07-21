d73d5fff793f5950621fc496983bcf6c46273a1f
Prajwol Acharya <87631485+prjla35@users.noreply.github.com>
2023-07-21 17:47:52 +0545

    Update particle.js

--- particle.js
+++ particle.js
@@ -1,137 +1,96 @@
-particlesJS("particles-js", {
-    
-    "particles": {
-      "number": {
-        "value": 100,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#fa0505"
-      },
-      "shape": {
-        "type": "square",
-        "stroke": {
-          "width": 1,
-          "color": "#fa0505"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 5,
-        "random": true,
-        "anim": {
-          "enable": true,
-          "speed": 5,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#FF0000",
-        "opacity": 4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 9,
-        "direction": "none",
-        "random": true,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": true,
-        "attract": {
-          "enable": true,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "window",
-      "events": {
-        "onhover": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "repulse"
+// Include particles.js library dynamically
+var script = document.createElement("script");
+script.src = "https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js";
+document.head.appendChild(script);
 
+// Delay to ensure the library is loaded
+setTimeout(function () {
+  particlesJS("particles-js", {
+    particles: {
+      number: {
+        value: 100,
+        density: {
+          enable: true,
+          value_area: 800,
         },
-        "resize": true
       },
-      "modes": {
-        "grab": {
-          "distance": 100,
-          "line_linked": {
-            "opacity": 5
-          }
+      color: {
+        value: "#FF5733", // Change the particle color to a more appealing color
+      },
+      shape: {
+        type: "circle", // Use circle shape for the particles
+        stroke: {
+          width: 0,
+          color: "#000000",
         },
-        "bubble": {
-          "distance": 405,
-          "size": 60,
-          "duration": 2,
-          "opacity": 10,
-          "speed": 3
+      },
+      opacity: {
+        value: 0.8, // Increase particle opacity for a softer effect
+        random: true,
+        anim: {
+          enable: true,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false,
         },
-        "repulse": {
-          "distance": 100,
-          "duration": 0.4
+      },
+      size: {
+        value: 5,
+        random: true,
+        anim: {
+          enable: true,
+          speed: 5,
+          size_min: 0.1,
+          sync: false,
         },
-        "push": {
-          "particles_nb": 4
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#FF0000",
+        opacity: 0.4, // Reduce the opacity of the particle lines
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 9,
+        direction: "none",
+        random: true,
+        straight: false,
+        out_mode: "out",
+        bounce: true,
+        attract: {
+          enable: true,
+          rotateX: 600,
+          rotateY: 1200,
         },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
+      },
     },
-    "retina_detect": true
-  
-});
-var count_particles, stats, update;
-stats = new Stats;
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function() {
-stats.begin();
-stats.end();
-if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-}
-requestAnimationFrame(update);
-};
-requestAnimationFrame(update);; // This is just a sample script. Paste your real code (javascript or HTML) here.
-
-if ('this_is' == /an_example/) {
-of_beautifier();
-} else {
-var a = b ? (c % d) : e[f];
-}
+    interactivity: {
+      detect_on: "window",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "grab", // Change onhover mode to "grab" for a more interactive experience
+        },
+        onclick: {
+          enable: true,
+          mode: "push", // Change onclick mode to "push" for a more interactive effect
+        },
+        resize: true,
+      },
+      modes: {
+        grab: {
+          distance: 200,
+          line_linked: {
+            opacity: 0.8, // Increase line opacity on grab
+          },
+        },
+        push: {
+          particles_nb: 4,
+        },
+      },
+    },
+    retina_detect: true,
+  });
+}, 1000); // Delay to ens
